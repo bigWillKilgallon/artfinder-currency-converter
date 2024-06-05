37cec8404401455e2b9ec1868105b6193ccea4c0
BigWill Kilgallon <_@bigwill.dev>
2024-06-05 05:11:23 +0100

    folder restructure and tailwind installation

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,31 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    root: true,
+    env: { browser: true, es2020: true },
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:react-hooks/recommended",
     ],
-  },
-}
+    ignorePatterns: ["dist", ".eslintrc.cjs"],
+    parser: "@typescript-eslint/parser",
+    plugins: ["react-refresh"],
+    rules: {
+        "react-refresh/only-export-components": [
+            "warn",
+            { allowConstantExport: true },
+        ],
+        "import/no-restricted-paths": [
+            "error",
+            {
+                zones: [
+                    // disables cross-feature imports:
+                    {
+                        target: "./src/features/currency-converter",
+                        from: "./src/features",
+                        except: ["./currency-converter"],
+                    },
+                ],
+            },
+        ],
+    },
+};
